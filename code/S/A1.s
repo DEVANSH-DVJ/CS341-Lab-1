13128f97472a6d678f65c1b6c009f1cbaba510f0
devansh-dvj <devansh.dvj@gmail.com>
2021-08-14 01:38:27 +0530

    Required strings added in data

--- code/S/A1.s
+++ code/S/A1.s
@@ -1,4 +1,14 @@
 # Length of longest zigzag subsequence
+.data
+
+size_string:
+    .asciiz "Enter the size of the array\n"
+
+elements_string:
+    .asciiz "Enter the elements of the array\n"
+
+end_string:
+    .asciiz "\n"
 .text
 
 main:
