413827aab10489daf00c4cd11a5fce9d3e4dedc0
devansh-dvj <devansh.dvj@gmail.com>
2021-08-13 22:01:49 +0530

    Finished after read, longest subsequence

--- compute_after_read.c
+++ compute_after_read.c
@@ -11,8 +11,26 @@ int main() {
   int arr[N];
   while (i < N) {
     scanf("%i", arr + i);
-    i++;
+    ++i;
   }
+
+  int prev_sign = 0, length = 1;
+
+  for (int i = 1; i < N; ++i) {
+    int curr_sign = 0;
+    if (arr[i] == arr[i - 1]) {
+      curr_sign = 0;
+    } else if (arr[i] > arr[i - 1]) {
+      curr_sign = 1;
+    } else {
+      curr_sign = -1;
+    }
+    if (curr_sign != prev_sign && curr_sign != 0) {
+      prev_sign = curr_sign;
+      ++length;
+    }
+  }
+  printf("%i\n", length);
 
   return 0;
 }
