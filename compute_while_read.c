1ea005befb1a780aa9dbcd5ce0ec5da559efcf65
devansh-dvj <devansh.dvj@gmail.com>
2021-08-13 22:39:41 +0530

    Finished while read, longest subsequence

--- compute_while_read.c
+++ compute_while_read.c
@@ -7,14 +7,41 @@ int main() {
   scanf("%i", &N);
 
   printf("Enter the elements of the array\n");
-  int prev=0, curr=0;
+  int prev = 0, curr = 0;
   scanf("%i", &curr);
   int i = 1;
+  int prev_sign = 0, curr_sign = 0;
+  int length = 1;
   while (i < N) {
     prev = curr;
+    prev_sign = curr_sign;
+
     scanf("%i", &curr);
-    i++;
+
+    if (prev == curr) {
+      curr_sign = 0;
+      ++i;
+      continue;
+    } else {
+
+      if (curr > prev) {
+        curr_sign = 1;
+      } else {
+        curr_sign = -1;
+      }
+
+      if (curr_sign == prev_sign) {
+        ++i;
+        continue;
+      } else {
+        ++length;
+        ++i;
+        continue;
+      }
+    }
   }
+
+  printf("%i\n", length);
 
   return 0;
 }
